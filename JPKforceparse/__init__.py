a86f53a262b15c3402a3326c57207331aef4781e
Ross Carter <ross.carter@jic.ac.uk>
2016-06-03 16:50:42 +0100

    package now generates force maps of height data

--- JPKforceparse/__init__.py
+++ JPKforceparse/__init__.py
@@ -1,9 +1,9 @@
 """JPK-force-parse package."""
 import numpy as np
 import matplotlib
-
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 __version__ = "0.0.1"
 __author__ = "Ross Carter"
@@ -43,6 +43,11 @@ class ForceCurve(object):
         self.seriesTime = data[:, 7]
         self.time = data[:, 8]
 
+        # Contact point calculation
+        min_tssep_value = min(i for i in self.tipSampleSep if i > 0)
+        contact_point_index = np.where(self.tipSampleSep == min_tssep_value)
+        self.contactpoint = self.height[contact_point_index]
+
     def plot_curve(self):
         plt.plot(self.smoothedMeasHeight, self.vDeflection)
         plt.show()
@@ -50,15 +55,39 @@ class ForceCurve(object):
 
 class ForceMap(object):
     def __init__(self, directory):
-        pass
+        print "Loading Curves... "
+        self.forcecurvedict = OrderedDict()
+
+        for i, curve_file in enumerate(os.listdir(directory)):
+            if i % 500 == 0:
+                print i, " curves loaded"
+            key = curve_file[-9:-4]
+            value = ForceCurve(os.path.join(directory, curve_file))
+            self.forcecurvedict[key] = value
+
+    def plot_height_map(self):
+        print "Plotting Height Map..."
+        x, y, z = list(), list(), list()
+        for curve in self.forcecurvedict.itervalues():
+            x.append(curve.x_pos)
+            y.append(curve.y_pos)
+            z.append(curve.contactpoint)
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.plot_trisurf(x, y, z, color=z, cmap='Set1', vmin=min(z), vmax=max(z))
+        # ax.plot_trisurf(x, y, z)
+        plt.show()
 
 
 def main():
-    path = "./example_data/Le Mannitol 010316 sample 1 map-data-2016.03.01-15.09.41.098_00013.txt"
-    path2 = "/Users/carterr/Documents/afm_andy/LE Mannitol 010316 Sample 1/Le Mannitol 010316 sample 1 map-data-2016.03.01-15.09.41.098_processed-2016.03.02-10.20.03/Le Mannitol 010316 sample 1 map-data-2016.03.01-15.09.41.098_00256.txt"
-    point1 = ForceCurve(path2)
-    point1.plot_curve()
-
+    # path = "./example_data/Le Mannitol 010316 sample 1 map-data-2016.03.01-15.09.41.098_00013.txt"
+    # path2 = "/Users/carterr/Documents/afm_andy/LE Mannitol 010316 Sample 1/Le Mannitol 010316 sample 1 map-data-2016.03.01-15.09.41.098_processed-2016.03.02-10.20.03/Le Mannitol 010316 sample 1 map-data-2016.03.01-15.09.41.098_00256.txt"
+    # point1 = ForceCurve(path2)
+    # point1.plot_curve()
+    dir = "/Users/carterr/Documents/afm_andy/LE Mannitol 010316 Sample 1/Le Mannitol 010316 sample 1 map-data-2016.03.01-15.09.41.098_processed-2016.03.02-10.20.03"
+    map1 = ForceMap(dir)
+    map1.plot_height_map()
 
 if __name__ == '__main__':
     main()
